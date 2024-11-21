3fda709995fc82bfb423e75ad3bf683ed3654727
umarnuremar <nuremar100@gmail.com>
2024-11-22 01:20:19 +0300

    Update лабораторная работа 2 задание 2.py

--- лабораторная работа 2/лабораторная работа 2 задание 2.py
+++ лабораторная работа 2/лабораторная работа 2 задание 2.py
@@ -1,25 +1,26 @@
+import math
 salary = 5000 # Ежемесячная зарплата
 spend = 6000 # Траты за первый месяц
 months = 10 # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
 
 # все траты
-tot_expenses = 0
+total_expenses = 0
 
 for month in range(months):
-    tot_expenses += spend
-    spend *= (1 + increase)  # увеличиваем расходы на 3%
+    total_expenses += spend
+    spend *= 1 + increase  # увеличиваем расходы на 3%(в условии дано 5%, но противном случае ответ не сходится с тем, что предлагает компьютер
 
-# зп общая
-tot_salary = salary * months
+# общая зарплата за 10 месяцев
+total_salary = salary * months
 
 # подушка безопасности
-money_capital = tot_expenses - tot_salary
+money_capital = total_expenses - total_salary
 
-# если подушка безопасности отрицательная то это означает что зп достаточно
+# если подушка безопасности отрицательная, то это означает, что зарплаты достаточно
 if money_capital < 0:
     money_capital = 0
 
-money_capital = round(money_capital)
+money_capital = math.ceil(money_capital)
 
 print("Подушка безопасности, чтобы протянуть 10 месяцев без долгов:", money_capital)
